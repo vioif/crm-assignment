718a10f628c72b7e71ee3f0a0652c13d9929a5f2
vioif <ireneaflores3@gmail.com>
2018-01-03 14:57:09 -0500

    initialize method in contact class

--- contact.rb
+++ contact.rb
@@ -1,14 +1,25 @@
 class Contact
 
+
+
   # This method should initialize the contact's attributes
-  def initialize
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name  = last_name
+    @email      = email
+    @notes      = notes
+
+    @@contacts = []
+    @@id = 1
 
   end
 
-  # This method should call the initializer, 
-  # store the newly created contact, and then return it
-  def self.create
 
+  # This method should call the initializer,
+  # store the newly created contact, and then return it
+  def self.create(first_name, last_name, email, note)
+    @@contacts << new_contact
+    return new_contact
   end
 
   # This method should return all of the existing contacts
@@ -22,7 +33,7 @@ class Contact
 
   end
 
-  # This method should allow you to specify 
+  # This method should allow you to specify
   # 1. which of the contact's attributes you want to update
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
@@ -43,6 +54,8 @@ class Contact
 
   end
 
+
+
   def full_name
 
   end
@@ -53,6 +66,27 @@ class Contact
 
   end
 
+  # READER
+
+    def first_name
+      @first_name
+    end
+
+    def last_name
+      @last_name
+    end
+
+    def email
+      @email
+    end
+
+    def note
+      @note
+    end
+
+    #WRITER
+
+
   # Feel free to add other methods here, if you need them.
-  
+
 end
