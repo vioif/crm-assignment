38eb5c3ca1cc71b15a7efdf4377ee1defc82ed3b
vioif <ireneaflores3@gmail.com>
2018-01-03 22:16:29 -0500

    added given crm methods and add require relative contact in main

--- crm.rb
+++ crm.rb
@@ -1,23 +1,60 @@
+require_relative 'contact'
+
 class CRM
 
-  def initialize
+  def initialize(name)
+    @name = name
 
   end
 
   def main_menu
-
+    while true # repeat indefinitely
+      print_main_menu
+      user_selected = gets.to_i
+      call_option(user_selected)
+    end
   end
 
   def print_main_menu
-
+    puts '[1] Add a new contact'
+    puts '[2] Modify an existing contact'
+    puts '[3] Delete a contact'
+    puts '[4] Display all the contacts'
+    puts '[5] Search by attribute'
+    puts '[6] Exit'
+    puts 'Enter a number: '
   end
 
   def call_option
-
+    case user_selected
+    when 1 then add_new_contact
+    when 2 then modify_existing_contact
+    when 3 then delete_contact
+    when 4 then display_all_contacts
+    when 5 then search_by_attribute
+    when 6 then exit_program  
+    # Finish off the rest for 3 through 6
+    # To be clear, the methods add_new_contact and modify_existing_contact
+    # haven't been implemented yet
+    end
   end
 
   def add_new_contact
+    p 'Enter First Name: '
+    first_name = gets.chomp
 
+    p 'Enter Last Name: '
+    last_name  = gets.chomp
+
+    p 'Enter Email Address: '
+    email      = gets,chomp
+
+    p 'Enter a Note: '
+    note       = gets.chomp
+
+    contact.create(first_name, last_name, email, note)
+
+    p 'Contact is now added'
   end
 
   def modify_existing_contact
@@ -29,11 +66,11 @@ class CRM
   end
 
   def display_all_contacts
-    
+
   end
 
   def search_by_attribute
-    
+
   end
 
 
